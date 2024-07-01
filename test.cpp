b22c84310a18888c884e4292df0a7f6660a79cd0
marg4ryn <poweski.jan@gmail.com>
2024-07-01 13:54:54 +0200

    Project created

---
+++ test.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+class Parser 
+{
+    static int lookahead;
+
+    public:
+        Parser() 
+        {
+            lookahead = std::cin.get();
+        }
+
+        void expr()
+        {
+            term();
+            while (true) 
+            {
+                if (lookahead == '+')
+                {
+                    match('+');
+                    term();
+                    std::cout << '+';
+                } else if (lookahead == '-')
+                {
+                    match('-');
+                    term();
+                    std::cout << '-';
+                } else return;
+            }
+        }
+
+        void term()
+        {
+
+        }
+
+        void match(int t)
+        {
+
+        }
+};
+
+int main() 
+{
+    printf("Hello, World!\n");
+    return 0;
+}
